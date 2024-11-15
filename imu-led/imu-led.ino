697ccb507b53d1c3bfe3ab719f36d5b5174201cd
Yashas Ambati <yambati3@gatech.edu>
2024-11-15 01:48:40 -0500

    IMU math to factor out effect of gravity

--- imu-led/imu-led.ino
+++ imu-led/imu-led.ino
@@ -2,12 +2,15 @@
 #include <Wire.h>
 
 #define LED_COUNT  16      // Number of LEDs in the ring
+#define G_MSS 9.81
+#define M_PI 3.14159
 
 Adafruit_NeoPixel left_ring(LED_COUNT, 9, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel right_ring(LED_COUNT, 10, NEO_GRB + NEO_KHZ800);
 
 float RateRoll, RatePitch, RateYaw;
 float AccX, AccY, AccZ;
+float AccX_no_gravity, AccY_no_gravity, AccZ_no_gravity;
 float AngleRoll, AnglePitch;
 float LoopTimer;
 
@@ -80,15 +83,21 @@ void gyro_signals(void) {
   RatePitch=(float)GyroY/65.5;
   RateYaw=(float)GyroZ/65.5;
 
-  AccX=(float)AccXLSB/4096;
-  AccY=(float)AccYLSB/4096;
+  AccX=-(float)AccXLSB/4096;
+  AccY=-(float)AccYLSB/4096;
   AccZ=(float)AccZLSB/4096;
 
-  AngleRoll=atan(AccY/sqrt(AccX*AccX+AccZ*AccZ))*1/(3.142/180);
-  AnglePitch=-atan(AccX/sqrt(AccY*AccY+AccZ*AccZ))*1/(3.142/180);
+  AngleRoll=-atan(AccY/sqrt(AccX*AccX+AccZ*AccZ));
+  AnglePitch=atan(AccX/sqrt(AccY*AccY+AccZ*AccZ));
+
+  AccX_no_gravity = AccX - sin(AnglePitch); // AccX - sin(AnglePitch);
+  AccY_no_gravity = AccY + sin(AngleRoll) * cos(AnglePitch);
+  AccZ_no_gravity = AccZ - cos(AngleRoll) * cos(AnglePitch);
+
+  AccX_no_gravity = AccX_no_gravity * G_MSS; // AccX_no_gravity * G_MSS;
+  AccY_no_gravity = AccY_no_gravity * G_MSS;
+  AccZ_no_gravity = AccZ_no_gravity * G_MSS;
 }
-
-
 
 void setup() {
   Serial.begin(57600);
@@ -110,22 +119,17 @@ JoyState get_joy_state() {
   // read joystick values and get next state.
   int joy_x = analogRead(JOY_X_PIN);
   int joy_y = analogRead(JOY_Y_PIN);
-
-
-
   int x_diff = abs(joy_x - 524);
   int y_diff = abs(joy_y - 524);
 
 
   // check if there is movement in the X direction.
   if (x_diff > y_diff){
-
     if (joy_x < 100) {
       return LEFT;
     } else if (joy_x > 900) {
       return RIGHT;
     }
-
   } else {
     if (joy_y < 100) {
       return UP;
@@ -134,8 +138,6 @@ JoyState get_joy_state() {
     }
   }
   return CENTER;
-
-  
 }
 
 unsigned long previousMillis = 0; 
@@ -143,15 +145,11 @@ const long blinkIntervalMillis = 500; // in mills
 bool isBlinkOn = false;  
 
 void loop() {
-  // Sensor Readings
   JoyState joy_state = get_joy_state();
   gyro_signals();
 
 
   /*
-  State Machine
-
-
   Transitions:
   -- Current System State, JoyState, Next System State
   - IDLE, LEFT, LEFT_BLINK;
@@ -161,8 +159,6 @@ void loop() {
   - RIGHT_BLINK, DOWN, IDLE; 
   - RIGHT_BLINK, LEFT, LEFT_BLINK
   */
-
-
 
   switch (state) {
     case IDLE:
@@ -191,7 +187,13 @@ void loop() {
   }
 
   updateTurnSignals();
-  updateBrakeLights(.1);
+  Serial.println(AccX_no_gravity);
+
+  if (AccX_no_gravity < -1) {
+    updateBrakeLights(abs(AccX_no_gravity / 9.81));
+  } else {
+    updateBrakeLights(0.0);
+  }
 }
 
 // pass in ring 
@@ -225,7 +227,6 @@ void updateTurnSignals() {
 }
 
 void updateBrakeLights(double light_intensity) {
-
   uint32_t color = left_ring.Color(255 * light_intensity, 0, 0);
   set_light(left_ring, LEFT_BRAKE_START, LEFT_BRAKE_END, color);
   set_light(right_ring, RIGHT_BRAKE_START, RIGHT_BRAKE_END, color);
