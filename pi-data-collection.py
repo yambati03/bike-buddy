40d5ea9370b597ca6bfbf8d3c829954f579acb3a
Abhiram Ghanta <abhiramg2021@gmail.com>
2024-11-18 20:20:51 -0500

    mpu6050 changes

--- pi-data-collection.py
+++ pi-data-collection.py
@@ -4,7 +4,7 @@ from mpu6050 import mpu6050
 def main():
     imu = mpu6050(0x68)
     # set low pass filter
-    imu.set_low_pass_filter(mpu6050.FILTER_BW_10)
+    imu.set_filter_range(mpu6050.FILTER_BW_10)
     imu.set_accel_range(mpu6050.ACCEL_RANGE_2G)
     imu.set_gyro_range(mpu6050.GYRO_RANGE_250DEG)
 
