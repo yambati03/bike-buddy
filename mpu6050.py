526c4795fa249adb714dc6a106bb2dc2a1263001
Abhiram Ghanta <abhiramg2021@gmail.com>
2024-11-18 19:49:54 -0500

    mpu6050 changes

--- mpu6050.py
+++ mpu6050.py
@@ -269,14 +269,14 @@ class mpu6050:
         return [accel, gyro, temp]
 
 
-if __name__ == "__main__":
-    mpu = mpu6050(0x68)
-    print(mpu.get_temp())
-    accel_data = mpu.get_accel_data()
-    print(accel_data["x"])
-    print(accel_data["y"])
-    print(accel_data["z"])
-    gyro_data = mpu.get_gyro_data()
-    print(gyro_data["x"])
-    print(gyro_data["y"])
-    print(gyro_data["z"])
+# if __name__ == "__main__":
+mpu = mpu6050(0x68)
+print(mpu.get_temp())
+accel_data = mpu.get_accel_data()
+print(accel_data["x"])
+print(accel_data["y"])
+print(accel_data["z"])
+gyro_data = mpu.get_gyro_data()
+print(gyro_data["x"])
+print(gyro_data["y"])
+print(gyro_data["z"])
