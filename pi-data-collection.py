91b1682d54ea6bf956b91b5900ae14d9d18595aa
Abhiram Ghanta <abhiramg2021@gmail.com>
2024-11-18 20:23:33 -0500

    updated data logger

--- pi-data-collection.py
+++ pi-data-collection.py
@@ -1,4 +1,5 @@
 from mpu6050 import mpu6050
+import time
 
 
 def main():
@@ -7,13 +8,17 @@ def main():
     imu.set_filter_range(mpu6050.FILTER_BW_10)
     imu.set_accel_range(mpu6050.ACCEL_RANGE_2G)
     imu.set_gyro_range(mpu6050.GYRO_RANGE_250DEG)
-
+    file_time = int(time.time())
+    print(file_time)
     while True:
         accel_data = imu.get_accel_data()
         gyro_data = imu.get_gyro_data()
-        print(f"Accel: {accel_data}, Gyro: {gyro_data}")
 
-        # time.sleep(1)
+        # save data to file
+        with open(f"data{file_time}.csv", "a") as f:
+            f.write(
+                f"{time.time(), gyro_data['x'], gyro_data['y'], gyro_data['z'], accel_data['x'], accel_data['y'], accel_data['z']}\n"
+            )
 
 
 if __name__ == "__main__":
