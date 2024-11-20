435e4135989eef628fe758eca6af4e679e04c5a2
Abhiram Ghanta <abhiramg2021@gmail.com>
2024-11-20 11:09:02 -0500

    remove gpio based file trigger

--- pi-data-collection.py
+++ pi-data-collection.py
@@ -1,13 +1,8 @@
-import RPi.GPIO as GPIO
-
 from mpu6050 import mpu6050
 import time
 
 
 def main():
-
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
     imu = mpu6050(0x68)
     # set low pass filter
@@ -19,11 +14,6 @@ def main():
     print(file_time)
 
     while True:
-
-        if GPIO.input(25):
-            file_time = int(time.time())
-            print(f"Button pressed, new file: data{file_time}.csv")
-            time.sleep(5)
 
         accel_data = imu.get_accel_data()
         gyro_data = imu.get_gyro_data()
