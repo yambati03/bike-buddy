a8e6e5c2132693b241029366b7b3e4ee937ce675
Abhiram Ghanta <abhiramg2021@gmail.com>
2024-11-19 22:18:22 -0500

    add GPIO signal stuff to PI

--- pi-data-collection.py
+++ pi-data-collection.py
@@ -1,16 +1,30 @@
+import RPi.GPIO as GPIO
+
 from mpu6050 import mpu6050
 import time
 
 
 def main():
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
     imu = mpu6050(0x68)
     # set low pass filter
     imu.set_filter_range(mpu6050.FILTER_BW_10)
     imu.set_accel_range(mpu6050.ACCEL_RANGE_2G)
     imu.set_gyro_range(mpu6050.GYRO_RANGE_250DEG)
     file_time = int(time.time())
+
     print(file_time)
+
     while True:
+
+        if GPIO.input(25):
+            file_time = int(time.time())
+            print(f"Button pressed, new file: data{file_time}.csv")
+            time.sleep(5)
+
         accel_data = imu.get_accel_data()
         gyro_data = imu.get_gyro_data()
 
